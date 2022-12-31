cfcce9edeb68496651c96656a8433197543b874c
Wambyat <anirudhaanekal@gmail.com>
2022-12-31 19:13:43 +0530

    added few tables

--- Testing/test_db.py
+++ Testing/test_db.py
@@ -2,39 +2,16 @@ import sqlite3
 
 try:
 
-    #TODO REMAKE BLOG_LIKE AND BLOG_FAV MAKE USER_FOLLOW
-
     conn = sqlite3.connect('test.db')
 
     cursor = conn.cursor()
-    print("Connecting to database")
+    print("\nConnecting to database\n")
 
-    query = "INSERT INTO user_info VALUES (4, 'John', 'John')"
-    cursor.execute(query)
-
-
-    query = "INSERT INTO blog_info VALUES (4, 'John''s First Blog', 1, 'This is John''s First Blog')"
-    cursor.execute(query)
-    
-    query = "INSERT INTO blog_like VALUES (1, 1)"
-    cursor.execute(query)
-    
-    query = "SELECT * FROM blog_like"
+    query = "SELECT sql FROM sqlite_master WHERE type='table'"
     cursor.execute(query)
     result = cursor.fetchall()
-    print(result)
-    
-    query = "SELECT * FROM blog_info"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print(result)
-    
-    query = "SELECT * FROM user_info"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    print(result)
-    #hmmmmmm wow new change
-
+    for i in result:
+        print(i)
 
 except Exception as e:
 
@@ -44,3 +21,4 @@ finally:
 
     conn.commit()
     conn.close()
+    print("\nConnection closed\n")
