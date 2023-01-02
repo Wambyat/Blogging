bcc9620b1a4f74e087a79934fda85d7840b9175e
Wambyat <anirudhaanekal@gmail.com>
2023-01-02 17:38:29 +0530

    asfdad

--- Testing/test_flask.py
+++ Testing/test_flask.py
@@ -1,24 +1,79 @@
 from flask import Flask, redirect, url_for, request, render_template
+import sqlite3
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 login_check = 0
+curr_user = ""
+#This function will take table name and required column as input. It will return a list of all values in that column.
 
+#TODO logout page, password reset and sign up logic and final htmls
+
+def sql_query(tbl , col):
+    
+    try:
+        conn = sqlite3.connect('test.db')
+        cursor = conn.cursor()
+        
+        print("\nConnecting to database\n")
+
+        query = "SELECT "+str(col)+" FROM "+str(tbl)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        
+    except Exception as e:
+
+        result = "Error"
+
+    finally:
+
+        conn.commit()
+        conn.close()
+        print("\nConnection closed\n")
+        return result
 
 @app.route('/login/',methods = ['POST','GET'])
 def login():
-    error = None
+    error = "We have Over 10 Users!"
     print("Login here")
     global login_check
+    global curr_user
+    if login_check == 1:
+        return render_template('logout.html', username = curr_user)
+
     if request.method == 'POST':
         
+        user_names = sql_query("user_info","name")
+
+        if type("abc") == type(user_names):
+            print(user_names)
+
+        else:
+
+            #This formats the result into a list of strings
+            user_names = [j for i in user_names for j in i]
+        
+        
+        passwords = sql_query("user_info","password")
+
+        if type("abc") == type(passwords):
+            print(passwords)
+
+        else:
+            #This formats the result into a list of strings
+            passwords = [j for i in passwords for j in i]
+        
+
         print("posting")
         user = request.form['username']
         password = request.form['password']
         
-        if user == "admin" and password == "admin":
+
+
+        if user in user_names and password == passwords[user_names.index(user)]:
             #Login success
             login_check = 1
+            curr_user = user
             print("login changed")
             return redirect(url_for('profile',name = user))
         
